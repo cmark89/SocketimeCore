d593e117170d7880441f8b72b70b4127f21481ea
cmark89 <cmark89@gmail.com>
2014-11-25 17:55:51 +0900

    Added command line arguments to client.py

--- client.py
+++ client.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 
 import socket
+import sys
 
+host = ""
+port = 0
+
+# Client program accepts up to 2 additional arguments
+# Usage: client.py [HOST] [PORT]
+# If host and port are not provided, it will default to local on
+# port 5700
+
+# Get the host to connect to
+if len(sys.argv) >= 2:
+	host = str(sys.argv[1])
+else:
+	host = socket.gethostname()	
+
+# Get the port to connect to 
+if len(sys.argv) >= 3:
+	port = int(sys.argv[2])
+else:
+	port = 5700					
+	
 s = socket.socket()			# Create the socket
-host = socket.gethostname()	# Connecting locally for test purposes
-port = 5700					# Get the port to connect to 
-
 s.connect((host, port))		# Initiate a connection (using a tuple)
 
 # Receive a message from the server and parse it to a string
